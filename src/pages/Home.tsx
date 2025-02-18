a9e4cfff90c63a8693ddea8c32cb394283171978
Abhishek Jaglan <jaglanabhishek08@gmail.com>
2025-02-18 12:45:06 +0530

    navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,25 @@
+import NavBar from "../components/NavBar"
+import SideBar from "../components/SideBar"
+
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        {/* Navbar */}
+        <div>
+            <NavBar/>
+        </div>
+        {/* Body below navbar */}
+        <div className="flex">
+            {/* SideBar */}
+            <div>
+                <SideBar/>
+            </div>
+            {/* URL Parsing Body */}
+            <div>
+
+            </div>
+        </div>
+    </div>
   )
 }
 
