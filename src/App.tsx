b5165302464d02dc00cbed6d680119518eecdb75
Abhishek Jaglan <jaglanabhishek08@gmail.com>
2025-02-18 09:15:47 +0530

    routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import LandingPage from "./pages/landingpage"
+import Home from "./pages/home"
+import SignUp from "./pages/signup"
+import SignIn from "./pages/signin"
+import UserInfo from "./pages/userinfo"
 
 function App() {
 
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/userinfo" element={<UserInfo/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
