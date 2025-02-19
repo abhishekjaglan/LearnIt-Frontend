b9b402c5cd034973a633453d22683342f1e698db
Abhishek Jaglan <jaglanabhishek08@gmail.com>
2025-02-19 10:56:05 +0530

    sidebar good look, not final

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,15 +1,28 @@
+import hamburgerIcon from '../assets/hamburger.svg'
+import UserInfoIcon from '../assets/user-16.png'
+import logOutIcon from '../assets/logout.png'
+
 function SideBar() {
   return (
     // Div wrapping the side bar
-    <div className="p-1 bg-black">
+    <div className="p-1 bg-black w-fit h-full fixed rounded-r-lg">
         {/* Main div for sidebar */}
-        <div className="flex-col space-y-20 w-sm bg-black fixed h-full border-2 border-white rounded-lg">
-          <div className="flex justify-start items-baseline space-x-2 bg-black mt-20 ml-10">
-            <img src="../assets/information.png" alt="user info" />
+        <div className="flex-col space-y-20 w-xs bg-black h-full border-2 border-white rounded-lg">
+          {/* Sidebar element - userinfo */}
+          <div className="flex justify-start bg-black mt-5 ml-10 w-fit rounded-lg hover:border-white hover:border-2 hover:rounded-lg">
+            <img src={hamburgerIcon} alt="hamburger" className='w-auto h-10' />
+          </div>
+          <div className="flex justify-start items-baseline space-x-10 mt-20 ml-10 ">
+            <div className=' bg-black  w-fit'>
+              <img src={UserInfoIcon} className='w-10 h-10'/>
+            </div>
             <p className="text-white font-bold text-2xl">User Info</p>
           </div>
-          <div className="flex justify-start items-baseline space-x-2 bg-black ml-10">
-            <img src="../assets/logout.png" alt="user info" />
+          {/* Sidebar element - logout */}
+          <div className="flex justify-start items-center space-x-10 bg-black ml-10">
+            <div className=' bg-black  w-fit '>
+            <img src={logOutIcon} className='w-10 h-10'/>
+            </div>
             <p className="text-white font-bold text-2xl">LogOut</p>
           </div>  
         </div>
@@ -18,3 +31,29 @@ function SideBar() {
 }
 
 export default SideBar
+
+// function SideBar() {
+//   return (
+//     // Outer wrapper div - using fit-content to match inner div size
+//     <div className="fixed h-full p-1 bg-black w-fit">
+//         {/* Main sidebar div */}
+//         <div className="flex-col w-64 bg-black h-[calc(100vh-8px)] border-2 border-white rounded-lg outline outline-2 outline-black outline-offset-2">
+//           {/* Sidebar content container */}
+//           <div className="flex flex-col space-y-20 p-10">
+//             {/* Sidebar element - userinfo */}
+//             <div className="flex items-center space-x-2">
+//               <img src="../assets/information.png" alt="user info" />
+//               <p className="text-white font-bold text-2xl">User Info</p>
+//             </div>
+//             {/* Sidebar element - logout */}
+//             <div className="flex items-center space-x-2">
+//               <img src="../assets/logout.png" alt="user info" />
+//               <p className="text-white font-bold text-2xl">LogOut</p>
+//             </div>
+//           </div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default SideBar
